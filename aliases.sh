12b4c82bb30ae203c5f87ee768161f249e00b558
Jack Duggan <74904632+jackjduggan@users.noreply.github.com>
2023-11-27 18:14:02 +0000

    Update aliases.sh
    fixed whitespace error

--- aliases.sh
+++ aliases.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 # Add your aliases here
 alias ll='ls -l'
 alias grep='grep --color=auto'
-
 # Define environment variables
 export greeting="Hello there!"
 export repos="/etc/yum.repos.d/"
